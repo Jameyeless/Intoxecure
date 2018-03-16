06a62eb778a7607f6b1ce0f56256a8cc02bd8ea0
danestarija <daveestarija_9@yahoo.com>
2018-03-16 15:44:08 +0800

    Initial commit

---
+++ app/src/main/java/com/intoxecure/intoxecure/IntoxecureService.java
@@ -0,0 +1,72 @@
+package com.intoxecure.intoxecure;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class IntoxecureService extends Service implements SensorEventListener {
+    public IntoxecureService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private Sensor mySensor;
+    private SensorManager SM;
+    private static final float SHAKE_THRESHOLD = 15.00f; // m/S**2
+    private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 1000;
+    private long mLastShakeTime;
+    public static float x, y, z;
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+            if ((curTime - mLastShakeTime) > MIN_TIME_BETWEEN_SHAKES_MILLISECS) {
+
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
+
+                double acceleration = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) - SensorManager.GRAVITY_EARTH;
+                Log.d("mySensor", "Acceleration is " + acceleration + "m/s^2");
+
+                if (acceleration > SHAKE_THRESHOLD) {
+                    mLastShakeTime = curTime;
+                    Toast.makeText(getApplicationContext(), "FALL DETECTED",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Start Detecting", Toast.LENGTH_LONG).show();
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //here u should make your service foreground so it will keep working even if app closed
+        return Service.START_STICKY;
+    }
+}
