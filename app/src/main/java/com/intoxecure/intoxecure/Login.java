7e7fe3401e00626d47919a48dee379a9197acfdf
EdRagasa <edd.ragasa@gmail.com>
2018-03-16 16:37:02 +0800

    Login Commit

--- app/src/main/java/com/intoxecure/intoxecure/Login.java
+++ app/src/main/java/com/intoxecure/intoxecure/Login.java
@@ -2,6 +2,9 @@ package com.intoxecure.intoxecure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.content.Intent;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void onLoginClick(View v) {
+        if(v.getId() == R.id.Blogin){
+            Intent i = new Intent(Login.this, main_page.class);
+            startActivity(i);
+        }
+    }
+
 }
