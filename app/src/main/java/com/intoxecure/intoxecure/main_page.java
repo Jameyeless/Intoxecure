8f588ef979dd43ac8f2546c0560dded34d8e2b8f
danestarija <dan.estarija@eee.upd.edu.ph>
2018-03-17 12:33:50 +0800

    Initial commit

---
+++ app/src/main/java/com/intoxecure/intoxecure/main_page.java
@@ -0,0 +1,16 @@
+package com.intoxecure.intoxecure;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by EdRagasa on 16/03/2018.
+ */
+
+public class main_page extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+}
