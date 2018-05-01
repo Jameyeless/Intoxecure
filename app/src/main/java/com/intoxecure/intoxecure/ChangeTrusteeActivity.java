511c043cf55247411e44d747f42f4a0386a8dc67
danestarija <dan.estarija@eee.upd.edu.ph>
2018-05-01 20:12:20 +0800

    Initial Commit

--- app/src/main/java/com/intoxecure/intoxecure/ChangeTrusteeActivity.java
+++ app/src/main/java/com/intoxecure/intoxecure/ChangeTrusteeActivity.java
@@ -1,7 +1,12 @@
 package com.intoxecure.intoxecure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
@@ -13,6 +18,7 @@ public class ChangeTrusteeActivity extends AppCompatActivity {
     ArrayAdapter adapter;
     ContactList contactList;
     ListView listView;
+    private static boolean toggleAll = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,11 +56,47 @@ public class ChangeTrusteeActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        contactList.SaveContactList(this, listView);
+        //contactList.SaveContactList(this, listView);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         contactList.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.change_trustee_menu, menu);
+        Log.d("OnCreateMenu", "test");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            /*
+            case R.id.selectAllButton:
+
+                if (toggleAll) {
+                    for (int i = 0; i < contactList.size(); i++) {
+                        ((ContactsArrayAdapter) listView.getAdapter()).setItemChecked(i, false);
+                    }
+                    toggleAll = false;
+                } else {
+                    for (int i = 0; i < contactList.size(); i++) {
+                        ((ContactsArrayAdapter) listView.getAdapter()).setItemChecked(i, true);
+                    }
+                    toggleAll = true;
+                }
+                Log.d("toggleAll", Boolean.toString(toggleAll));
+                return true;*/
+            case R.id.setButton:
+                contactList.SaveContactList(this, listView);
+                startActivity(new Intent(this, ProfileActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
